faf1aef1299a0f1dc2c2e37b99395b97ebc1bdac
mcqeems <mcqeemsofficial@gmail.com>
2025-05-15 09:44:06 +0700

    Added Hapi and Fixed Route

---
+++ src/handler.js
@@ -0,0 +1,14 @@
+const { nanoId } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = () => {};
+
+const getAllBooksHandler = () => {};
+
+const getBookByIdHandler = () => {};
+
+const editBookByIdHandler = () => {};
+
+const deleteBookByIdHandler = () => {};
+
+module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler };
