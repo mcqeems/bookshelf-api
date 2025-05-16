9c85a32843005320e517ffe10218d1f2ecfd0e61
mcqeems <mcqeemsofficial@gmail.com>
2025-05-16 17:40:53 +0700

    added delete handler

--- src/handler.js
+++ src/handler.js
@@ -125,9 +125,83 @@ const getBookByIdHandler = (request, h) => {
   return response;
 };
 
-const editBookByIdHandler = () => {};
+const editBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
 
-const deleteBookByIdHandler = () => {};
+  const updatedBook = request.payload;
+
+  if (!updatedBook.name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku, mohon isi nama buku.',
+    });
+    response.code(400);
+    return response;
+  }
+
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index === -1) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memeperbarui buku. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+  }
+
+  const oldBook = books[index];
+
+  const mergedBook = {
+    ...oldBook,
+    ...updatedBook,
+  };
+
+  if (mergedBook.readPage > mergedBook.pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku, readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  const updatedAt = new Date().toISOString();
+  const finished = mergedBook.readPage === mergedBook.pageCount;
+
+  books[index] = mergedBook;
+
+  const response = h.response({
+    status: 'success',
+    message: 'Buku berhasil diperbarui',
+  });
+  response.code(200);
+  return response;
+};
+
+const deleteBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index === -1) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Buku gagal dihapus, Id tidak ditemukan',
+    });
+    response.code(400);
+    return response;
+  }
+
+  books.splice(index, 1);
+
+  const response = h.response({
+    status: 'success',
+    message: 'Buku berhasil dihapus',
+  });
+  response.code(200);
+  return response;
+};
 
 module.exports = {
   addBookHandler,
